07dac2ca1caea7fc7b886faf648c0c108c0eda6a
Jonnathan Campoberde <jonnathanca9601@gmail.com>
2019-02-25 21:22:40 -0500

    Actualización

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to presentacion-angular!');
+    expect(page.getTitleText()).toEqual('Bienvenido a la presentación de Angular');
   });
 });
